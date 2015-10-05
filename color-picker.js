009e8a93418e081901925741b8c02400065e65d2
yizhan wang <yizhanw@gmail.com>
2015-10-05 22:30:21 +1300

    add countinue generate pixelData

--- color-picker.js
+++ color-picker.js
@@ -1,13 +1,13 @@
 /**
  * Created by user on 3/10/15.
  */
-
+"use strict";
 // from stack over flow: http://stackoverflow.com/questions/8751020/how-to-get-a-pixels-x-y-coordinate-color-from-an-image
 //
 
 // Target: angular directive?
 
-// TODO: add angular 
+// TODO: add angular
 var canvas = document.getElementById('#my-canvas');
 var context = canvas.getContext('2d');
 
@@ -21,3 +21,16 @@ canvas.addEventListener('click', function(event) {
     var pixelData = canvas.getContext('2d').getImageData(event.offsetX, event.offsetY, 1, 1).data;
     console.log('pixelData', pixelData);
 });
+
+var trackMouseMove = (Element) => {
+    // console.log('tracking mouse move', event);
+    Element.addEventListener('mousemove', (event) => {
+        let pixelData = canvas.getContext('2d').getImageData(event.offsetX, event.offsetY, 1, 1).data;
+        console.log('pixelData', pixelData);
+    })
+};
+
+var killTrackMouseMove = (event) => {
+    console.log('kill tracking mouse move');
+    event.removeEventListener('mousemove')
+};
